9ccf024ea5b1e76520a26feb07b58edead63b488
Mo Sahil <118831286+mo-sahil@users.noreply.github.com>
2025-01-27 00:06:05 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ const config = {
   mongoURI: process.env.MONGODB_URI,
   grpcPort: process.env.GRPC_PORT || 50051,
   expressPort: process.env.PORT || 3000,
-  corsOrigins: ["http://0.0.0.0:0", "http://localhost:5173"], ["https://quest-search-chi.vercel.app/"],
+  corsOrigins: ["http://0.0.0.0:0", "http://localhost:5173", "https://quest-search-chi.vercel.app/"],
 };
 
 module.exports = config;
